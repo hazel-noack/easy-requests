bee6fb40a3d2fcef8214e818603138d1cf364061
Hazel Noack <hazel@webcontact.de>
2025-06-11 12:44:57 +0200

    implemented cache

--- python_requests/connections.py
+++ python_requests/connections.py
@@ -1,13 +1,22 @@
 from __future__ import annotations
 from typing import Optional
 import requests
+from datetime import timedelta
 from urllib.parse import urlparse, urlunsplit, ParseResult
 
 from . import cache
 
 class Connection:
-    def __init__(self, session: Optional[requests.Session] = None) -> None:
+    def __init__(
+        self, 
+        session: Optional[requests.Session] = None,
+        cache_enable: bool = True,
+        cache_expires_after: Optional[timedelta] = None
+    ) -> None:
         self.session = session if session is not None else requests.Session()
+
+        self.cache_enable = cache_enable
+        self.cache_expires_after = cache_expires_after if cache_expires_after is not None else timedelta(hours=1)
 
     def generate_headers(self, referer: Optional[str] = None):
         headers = {
@@ -21,10 +30,32 @@ class Connection:
 
         self.session.headers.update(**headers)
 
-    def get(self, url: str):
-        if cache.has_cache(url):
+    def send_request(self, request: requests.Request) -> requests.Response:
+        url = request.url 
+        if url is None:
+            raise ValueError("can't send a request without url")
+        if self.cache_enable and cache.has_cache(url):
             return cache.get_cache(url)
+        
+        response = self.session.send(request.prepare())
+        if self.cache_enable:
+            cache.write_cache(url, response)
+        return response
 
-        resp = self.session.get(url)
-        cache.write_cache(url, resp)
-        return resp
+
+    def get(self, url: str, headers: Optional[dict] = None, **kwargs) -> requests.Response:
+        return self.send_request(requests.Request(
+            'GET',
+            url=url,
+            headers=headers,
+            **kwargs
+        ))
+    
+    def post(self, url: str, headers: Optional[dict] = None, json: Optional[dict] = None, **kwargs) -> requests.Response:
+        return self.send_request(requests.Request(
+            'POST',
+            url=url,
+            headers=headers,
+            json=json,
+            **kwargs,
+        ))
