a31a172557fd932a761f2efa9178d676b728034c
Hazel <hazel@noreply.gitea.elara.ws>
2025-06-11 08:57:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Python-Requests
+
+A Python library for simplified HTTP requests, featuring rate limiting, browser-like headers, and automatic retries. Built on the official `requests` library for reliability.
+
+## License
+
+This project is licensed under the [**🏳️‍🌈 Opinionated Queer License v1.2**](https://oql.avris.it/license). So use is strictly prohibited for cops, military and everyone who actively works against human rights.
---
+++ python_requests/__main__.py
@@ -0,0 +1,34 @@
+import argparse
+import logging
+
+
+def cli():
+    parser = argparse.ArgumentParser(
+        description="A Python library for simplified HTTP requests, featuring rate limiting, browser-like headers, and automatic retries. Built on the official `requests` library for reliability.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Sets the logging level to debug."
+    )
+
+    args = parser.parse_args()
+
+    # Configure logging based on the debug flag
+    if args.debug:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+        logging.debug("Debug logging enabled")
+    else:
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+
+if __name__ == "__main__":
+    cli()
