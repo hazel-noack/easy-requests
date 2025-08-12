dc48753b66f65a6357bb2f5eb9e6e7fcb6508a1d
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-08-12 12:45:36 +0200

    functionality to generate refere automatically

--- easy_requests/connections.py
+++ easy_requests/connections.py
@@ -137,7 +137,8 @@ class Connection:
                 "\turl           = %s\n"
                 "\tcache_enabled = %s\n"
                 "\tmax_retries   = %s\n"
-            ),
+                "\t[headers]\n"
+            ) + "".join(f"\t\t{key} = {val}\n" for key, val in request.headers.items()),
             url_hash,
             request.method,
             url,
