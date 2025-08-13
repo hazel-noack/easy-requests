2b60e4ae51e6ebba05513f2c4e249921c9067c63
Hazel Noack <223855870+hazel-noack@users.noreply.github.com>
2025-08-13 13:30:01 +0200

    playground

--- playground.py
+++ playground.py
@@ -8,8 +8,13 @@ from easy_requests import Connection, SilentConnection, init_cache
 
 
 if __name__ == "__main__":
-    c = Connection(warning_status_codes={200,}, cache_enabled=False, request_delay=1)
-    r = c.get("https://google.com")
-    r = c.get("https://google.com")
+    c = Connection(        
+        request_delay=.3,
+        additional_delay_per_try=1.5,
+        warning_status_codes={403, },
+        max_retries=1000
+    )
+    c.generate_headers()
+    r = c.get("https://www.reddit.com/user/DasPrivate0/submitted.json?sort=new&limit=100&after=&count=0")
 
     print(r.status_code)
